57a19e20ac717132e6d228f53ee679f4092de6ac
David Rajcher <davidrajcher@Davids-MacBook-Pro-2.local>
2022-10-04 14:27:03 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import characters from "./data/characters.json";
 
-function App() {
+const App = () => {
+  const [loggedCharacter, setLoggedCharacter] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <nav>
+        <div class='avatar'>
+          <img src={characters[loggedCharacter].image} />
+          <p>David Rajcher</p>
+        </div>
+        <button class='logout'>Logout</button>
+      </nav>
+      {!loggedCharacter ? (
+        <div></div>
+      ) : (
+        <div id='main'>
+          <section id='chosen-container'></section>
+          <section id='character-container'>
+            {characters.map((character) => (
+              <div class='card'>
+                <img src={character.image} alt='character' />
+                <div className='card-container'>
+                  <h2>{character.name}</h2>
+                  <p>
+                    <span>Sex:</span> {character.gender}
+                  </p>
+                  <p>
+                    <span>Status:</span> {character.status}
+                  </p>
+                  <p>
+                    <span>Species:</span> {character.species}
+                  </p>
+                </div>
+                <div class='button-container'>
+                  <button class='choose-button'>Choose</button>
+                </div>
+              </div>
+            ))}
+          </section>
+        </div>
+      )}
+      <aside>
+        <form>
+          <input name='search' id='search' placeholder='search' />
+          <label for='gender'>
+            Gender
+            <select name='gender' id='gender'></select>
+          </label>
+          <label for='status'>
+            Status
+            <select name='status' id='status'></select>
+          </label>
+          <label for='species'>
+            Species
+            <select name='species' id='species'></select>
+          </label>
+        </form>
+      </aside>
+
+      <footer>
+        <img src='../assets/avatar.png' />
+        <p>Created by David L. Rajcher</p>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
